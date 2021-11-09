f0940e73f90b25721d87a8e044c11f75a3ad0040
oko-ha <73021910+oko-ha@users.noreply.github.com>
2021-11-09 16:22:13 +0900

    Delete 카카오 2021 채용연계형 인턴십 directory

--- 카카오 2021 채용연계형 인턴십/kakao1.py
+++
@@ -1,9 +0,0 @@
-import re
-def solution(s):
-    num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i in range(10):
-        s = s.replace(num[i], str(i))
-    return int(s)
-
-s = "one4seveneight"
-print(solution(s))
